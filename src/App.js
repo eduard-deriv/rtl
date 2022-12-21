16bc2a3033bfe0152bf81fcd163e65e3749e7e4e
Eduard Hrachou <eduard@regentmarkets.com>
2022-12-21 11:37:35 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Types from "./components/Types";
+import SearchAndAssertive from "./components/SearchAndAssertive";
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Types/>
+            <SearchAndAssertive/>
+        </>
     );
 };
 
